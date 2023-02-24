dfbc47368abe8f1dd04254d3f035cce45ddbfc98
Felix <felix.lerner01@gmail.com>
2023-02-24 12:30:54 -0300

    Reorder folders

---
+++ Solid/Single Responsability/Single_broken.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Solid_L.Liskov_broken;
+
+namespace Solid.Single_Responsability
+{
+    public class Single_broken
+    {
+     /*   static void Main(string[] args)
+        {
+            
+        }*/
+    }
+}
