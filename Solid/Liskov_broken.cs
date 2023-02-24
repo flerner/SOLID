e2abe1f42a943bf4b7dfe344a63c6e3fde92d1a1
Felix <felix.lerner01@gmail.com>
2023-02-24 12:23:54 -0300

    First commit

---
+++ Solid/Liskov_broken.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid_L
+{
+    public class Liskov_broken
+    {
+        //descomentar éste main y comentar el de Liskov_ok para probar
+        /*static void Main(string[] args)
+        {          
+            AbstractSale abstractSale = new Sale(12, "Felix", 0.40m);
+            abstractSale.CalculateTaxes();
+            abstractSale.Generate();
+            Console.ReadKey();
+        }*/
+        public abstract class AbstractSale
+        {
+            protected decimal amount;
+            protected string customer;
+            protected decimal taxes;
+
+            public abstract void Generate();
+            public abstract void CalculateTaxes();
+        }
+  
+       public class Sale : AbstractSale
+        {
+            public Sale(decimal amount, string customer, decimal taxes)
+            {
+                this.amount = amount;
+                this.customer = customer;
+                this.taxes = taxes;
+            }
+
+            public override void CalculateTaxes()
+            {
+                Console.WriteLine("Se calculan los impuestos");
+            }
+
+            public override void Generate()
+            {
+                Console.WriteLine("Se genera la venta");
+            }
+        }
+        //en este punto es en donde se rompe la regla de Liskov, ya que tenemos un campo extra que seria el impuesto...
+        public class TaxFreeSale: AbstractSale
+        {
+            public TaxFreeSale(decimal amount, string customer)
+            {
+                this.amount = amount;
+                this.customer = customer;
+                this.taxes = 0;
+            }
+
+            public override void CalculateTaxes()
+            {
+                throw new NotImplementedException();
+            }
+
+            public override void Generate()
+            {
+                Console.WriteLine("Se genera la venta");
+            }
+        }
+    }
+}
---
+++ Solid/Liskov_ok.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid_L
+{
+    class Liskov_ok
+    {
+        static void Main(string[] args)
+        {
+            AbstractSaleWithTaxes abstractSale = new SaleWithTaxes(12, "Felix", 0.40m);
+            abstractSale.CalculateTaxes();
+            abstractSale.Generate();
+            AbstractSale abstractSaleTaxFree = new TaxFreeSale(12, "Felix");
+            abstractSale.Generate();
+
+            
+            Console.ReadKey();
+        }
+        public abstract class AbstractSale
+        {
+            protected string customer;
+            protected decimal amount;
+
+            public abstract void Generate();
+        }
+        public abstract class AbstractSaleWithTaxes: AbstractSale
+        {
+            protected decimal taxes;
+
+            public abstract void CalculateTaxes();
+        }
+
+        public class SaleWithTaxes : AbstractSaleWithTaxes
+        {
+            public SaleWithTaxes(decimal amount, string customer, decimal taxes)
+            {
+                this.amount = amount;
+                this.customer = customer;
+                this.taxes = taxes;
+            }
+
+            public override void CalculateTaxes()
+            {
+                Console.WriteLine("Se calculaan los impuestos");
+            }
+
+            public override void Generate()
+            {
+                Console.WriteLine("Se genera la venta");
+            }
+        }
+        //Ahora si se está cumpliendo ya que se plantea correctamente la jerarquía, en caso de no necesitar taxes, no dejamos ningun campo ni metodo sin implementar
+        //sino que heredamos de una clase abstracta mas genérica.
+        public class TaxFreeSale : AbstractSale
+        {
+            public TaxFreeSale(decimal amount, string customer)
+            {
+                this.amount = amount;
+                this.customer = customer;
+            }
+
+            public override void Generate()
+            {
+                Console.WriteLine("Se genera la venta");
+            }
+        }
+    }
+}
